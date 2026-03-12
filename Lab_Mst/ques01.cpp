e514c8db7bafa97fe621481cb6c39427addef061
Nakul Kumar <149577160+Nakul-Kumar01@users.noreply.github.com>
2026-03-12 14:12:23 +0530

    Lab_Mst

---
+++ Lab_Mst/ques01.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main() {
+    int n;
+    cin>>n;
+
+    vector<int>arr(n);
+    int l=1,h =0;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        h = max(h,arr[i]);
+    }
+
+    int maxoper;
+    cin>>maxoper;
+    int ans =0;
+
+    while(l<=h){
+        int mid = (l+h)/2;
+        int count =0;
+        for(auto x: arr) count = count + (x-1)/mid;
+
+        if(count <= maxoper){
+            ans = min(ans,mid);
+            h = mid -1;
+        }
+        else l = mid +1;
+
+    }
+    cout<<ans;
+
+    return 0;
+}
